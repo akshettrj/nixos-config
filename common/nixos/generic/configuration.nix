4aae7fee11bc98cee04e2331d551c28792333308
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-03-12 06:12:53 +0000

    [nix-community] add binary cache

--- common/nixos/generic/configuration.nix
+++ common/nixos/generic/configuration.nix
@@ -110,6 +110,12 @@
       settings = {
         experimental-features = "nix-command flakes";
         auto-optimise-store = true;
+        substituters = [
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
       };
       gc = lib.mkIf config.garbageCollection.enable {
         automatic = true;
