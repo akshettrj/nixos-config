b56177e28d85e8c68321c2c67b5173b645d0647c
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-03-12 06:10:49 +0000

    [lf] update config

--- common/home-manager/modules/file_explorers/lf.nix
+++ common/home-manager/modules/file_explorers/lf.nix
@@ -18,6 +18,31 @@
       "<enter>" = "shell";
       x = ''''$"$f"'';
       X = ''!"$f"'';
+      J = '':updir; set dironly true; down; set dironly false; open'';
+      K = '':updir; set dironly true; up; set dironly false; open'';
+      DD = "delete";
     };
+    commands = {
+      # Basics
+      mkdir = ''%mkdir "$@"'';
+      touch = ''%touch "$@"'';
+
+      # Zoxide
+      z = ''
+        %{{
+          result="$(${pkgs.zoxide}/bin/zoxide query --exclude $PWD $@)"
+          lf -remote "send $id cd $result"
+        }}
+      '';
+      zi = ''
+        ''${{
+          result="$(zoxide query -i)"
+          lf -remote "send $id cd $result"
+        }}
+      '';
+    };
+    extraConfig = ''
+      %[ $LF_LEVEL -eq 1 ] || echo "Warning: You're in a nested lf instance! [LEVEL $LF_LEVEL]"
+    '';
   };
 }
