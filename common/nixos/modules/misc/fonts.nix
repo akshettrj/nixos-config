505da28f8ef6a61c717fbb40eaf7097af19ad6a5
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-04-30 21:21:21 +0530

    [fonts] add fontconfig for telegram

--- common/nixos/modules/misc/fonts.nix
+++ common/nixos/modules/misc/fonts.nix
@@ -37,5 +37,49 @@
             liberation_ttf
         ];
 
+        fonts.fontconfig = {
+            enable = true;
+            defaultFonts = {
+                emoji = [ "Noto Color Emoji" ];
+                monospace = [ "${pro_theming.fonts.main.name}" ];
+                sansSerif = [ "${pro_theming.fonts.main.name}" ];
+                serif = [ "${pro_theming.fonts.main.name}" ];
+            };
+            localConf = ''
+                <fontconfig>
+                <match target="pattern">
+                <test qual="any" name="family"><string>monospace</string></test>
+                <edit name="family" mode="assign" binding="same"><string>${pro_theming.fonts.main.name}</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>${(builtins.elemAt pro_theming.fonts.backups 0).name}</string></edit>
+                </match>
+
+                <match target="pattern">
+                <test qual="any" name="family"><string>ui-monospace</string></test>
+                <edit name="family" mode="assign" binding="same"><string>${pro_theming.fonts.main.name}</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>${(builtins.elemAt pro_theming.fonts.backups 0).name}M</string></edit>
+                </match>
+
+                <match target="pattern">
+                <test qual="any" name="family"><string>serif</string></test>
+                <edit name="family" mode="assign" binding="same"><string>${pro_theming.fonts.main.name}</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>${(builtins.elemAt pro_theming.fonts.backups 0).name}M</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>Noto Serif</string></edit>
+                </match>
+
+                <match target="pattern">
+                <test qual="any" name="family"><string>sans-serif</string></test>
+                <edit name="family" mode="assign" binding="same"><string>${pro_theming.fonts.main.name}</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>${(builtins.elemAt pro_theming.fonts.backups 0).name}M</string></edit>
+                <edit name="family" mode="append" binding="weak"><string>Noto Serif</string></edit>
+                </match>
+
+                <match target="pattern">
+                <test qual="any" name="family"><string>emoji</string></test>
+                <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+                </match>
+                </fontconfig>
+                '';
+        };
+
     };
 }
