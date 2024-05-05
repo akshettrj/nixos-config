18c93269e5a21e696647a99dd501e1859de31239
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-05-05 23:09:01 +0530

    [bash] add basic bash config

--- common/home-manager/modules/shells/bash.nix
+++ common/home-manager/modules/shells/bash.nix
@@ -1,13 +1,51 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
     config = let
 
         pro_shells = config.propheci.shells;
+        pro_file_explorers = config.propheci.programs.file_explorers;
 
     in lib.mkIf pro_shells.bash.enable {
 
-        programs.bash.enable = true;
+        programs.bash = {
+            enable = true;
+
+            enableVteIntegration = true;
+            enableCompletion = true;
+
+            historyControl = ["erasedups" "ignorespace"];
+            historyFile = "$HOME/.cache/bash_history";
+            historyIgnore = [
+                "reboot*"
+                "shutdown*"
+                ". *"
+            ] ++ lib.optionals pro_shells.zoxide.enable [
+                "z"
+                "z *"
+                "zi"
+                "zi *"
+            ];
+            historySize = 10000;
+
+            initExtra = ''
+            '' + lib.optionals pro_file_explorers.lf.enable ''
+
+                ###################################################
+
+                # LFCD
+                function lfcd() {
+                    tmp="$(mktemp)"
+                    ${pkgs.lf}/bin/lf -last-dir-path="$tmp" "$@"
+                    if [ -f "$tmp" ]; then
+                        dir="$(cat "$tmp")"
+                        rm -f "$tmp" >/dev/null
+                        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+                    fi
+                }
+
+            '';
+        };
 
     };
 
