ef2e8fe896eb8af4f8ae572930e1e4efd2782672
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-26 16:58:19 +0530

    aliases: add aliases for nh commands

--- common/home-manager/modules/shells/aliases.nix
+++ common/home-manager/modules/shells/aliases.nix
@@ -2,4 +2,6 @@
     cp = "cp -rvi";
     rm = "rm -vi";
     rsync = "rsync -urvP";
+    nh-switch = "nh os switch ~/.config/nixos-flake -- --no-eval-cache --accept-flake-config --show-trace";
+    nh-boot = "nh os boot ~/.config/nixos-flake -- --no-eval-cache --accept-flake-config --show-trace";
 }
